ae342eb37d74feb19c45545fe12b901c89d467bd
P.R. Gupte <p.r.gupte@rug.nl>
2020-11-16 15:11:15 +0100

    no website

--- render_books.sh
+++ render_books.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-Rscript --slave -e 'bookdown::render_book("index.rmd")'
+
 Rscript --slave -e 'bookdown::render_book("index.rmd", "rmarkdown::pdf_document"); warnings()'
 
 # to render source files
