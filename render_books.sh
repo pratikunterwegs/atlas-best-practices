e7bb59ce462bbb9b304b705fa8572d384f9942fe
Pratik Gupte <pratikgupte16@gmail.com>
2020-11-22 22:10:32 +0100

    book rendering corrections

--- render_books.sh
+++ render_books.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Rscript --slave -e 'bookdown::render_book("index.rmd", "rmarkdown::pdf_document"); warnings()'
+Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_document2"); warnings()'
 
 # to render source files
-Rscript --slave -a 'lapply(list.files(pattern = "\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
+Rscript --slave -e 'lapply(list.files(pattern = "\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
