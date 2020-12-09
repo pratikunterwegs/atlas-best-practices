75731f181db4d75fd0fad32c76ab89d0f05ff870
Pratik Gupte <pratikgupte16@gmail.com>
2020-12-09 16:21:32 +0100

    build and name supplement correctly

--- render_books.sh
+++ render_books.sh
@@ -4,3 +4,11 @@ Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_document2"
 
 # to render source files
 Rscript --slave -e 'lapply(list.files(pattern = "\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
+
+# copy pdfs and name correctly
+cp -p atlas-manuscript/arxiv_article.pdf docs/ms_atlas_preproc_`date -I`.pdf
+cp -p docs/supplementary_material.pdf docs/supplementary_material_`date -I`.pdf
+rm docs/supplementary_material.pdf
+
+# build atlastools manual
+Rscript --slave -e 'devtools::build_manual(pkg = "../atlastools", path = "docs/")'
