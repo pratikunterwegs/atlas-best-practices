96f062e0fa2857d672a627b8c88ce3db6f4aaf14
Pratik Gupte <pratikgupte16@gmail.com>
2021-02-01 13:53:52 +0100

    edit doc name

--- render_books.sh
+++ render_books.sh
@@ -10,7 +10,7 @@ Rscript --slave -e 'bookdown::render_book("index.rmd", "bookdown::pdf_document2"
 Rscript --slave -e 'lapply(list.files(pattern = "\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
 
 # copy pdfs and name correctly
-cp -p atlas-manuscript/arxiv_article.pdf docs/ms_atlas_preproc_main_text`date -I`.pdf
+cp -p atlas-manuscript/arxiv_article.pdf docs/ms_atlas_preproc_main_text_`date -I`.pdf
 mv docs/supplementary_material.pdf docs/ms_atlas_preproc_supplementary_material_`date -I`.pdf
 
 # copy tex and replace old tex
