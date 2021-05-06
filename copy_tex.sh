31bc9d515835f832368918fc6c86097058dbe40c
Pratik Gupte <pratikgupte16@gmail.com>
2021-05-06 10:17:42 +0200

    no copy tex

--- copy_tex.sh
+++ copy_tex.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# copy tex and replace old tex
-cp -p atlas-manuscript/arxiv_article.tex docs/ms_atlas_preproc_main_text.tex
-
+# transfer figures
 cp figures/fig_0*.png atlas-manuscript/figures
