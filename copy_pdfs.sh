ea06690890563d6305faa0bfa049cba6cec5d2fc
Pratik Gupte <pratikgupte16@gmail.com>
2021-05-21 17:30:40 +0200

    makes docs

---
+++ copy_pdfs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# remove old tex files and pdfs
+
+cd docs
+rm *.pdf
+
+cd ..
+
+# copy pdfs and name correctly
+cp -p atlas-manuscript/arxiv_article.pdf docs/ms_atlas_preproc_main_text_`date -I`.pdf
+
+pandoc atlas-manuscript/arxiv_article.tex --bibliography=atlas-manuscript/references.bib -o docs/ms_preprocessing_`date -I`.docx
