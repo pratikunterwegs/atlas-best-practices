204922ee9a139945fc47fa9b43aaead778ae4330
Pratik Gupte <pratikgupte16@gmail.com>
2021-09-09 12:57:56 +0200

    do not render supplement as scripts

--- bash/render_books.sh
+++ bash/render_books.sh
@@ -14,7 +14,7 @@ Rscript --vanilla --slave -e 'styler::style_dir("supplement", filetype = "Rmd", 
 Rscript --vanilla --slave -e 'bookdown::render_book("supplement/")'
 
 # to render source files
-Rscript --slave -e 'lapply(list.files(pattern = "supplement\\/\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
+# Rscript --slave -e 'lapply(list.files(pattern = "supplement\\/\\d{2}\\w+.Rmd"), function(x) knitr::purl(x, output = sprintf("R/%s", gsub(".{4}$", ".R", x))))'
 
 mv docs/supplementary_material.pdf docs/ms_atlas_preproc_supplementary_material_`date -I`.pdf
 
